bdab5c6ac1acaf8f14aaa05bac2ae2059b13d3ba
Nitin Sigilipelli <sigilipellinitin@gmail.com>
2026-03-11 22:01:08 +0530

    routing changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import InputPage from "./pages/InputPage";
 import OutputPage from "./pages/OutputPage";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
-        <Route path="/" element={<Home />} />
+
+        {/* Default route */}
+        <Route path="/" element={<Navigate to="/input" />} />
+
+        {/* Input terminal */}
         <Route path="/input" element={<InputPage />} />
+
+        {/* Output terminal */}
         <Route path="/output" element={<OutputPage />} />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
